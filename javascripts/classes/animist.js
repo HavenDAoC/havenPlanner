bf37bfb25327ab286855b2654ecd5b456acb204f
deftness <leftygamer02@pm.me>
2023-03-28 13:31:17 -0600

    Began updating formatting and adding in-code documentation

--- javascripts/classes/animist.js
+++ javascripts/classes/animist.js
@@ -1,14 +1,78 @@
-// --------------------------------------------------------------------------------
-// ANIMIST - HIBERNIA
-// Class based upon v1.65 patch
-// --------------------------------------------------------------------------------
+/*! ************************************************************************
+DAOC CHARACTER BUILDER - A static website for planning DAoC character specs
 
+This website is based upon the Uthgard Character Builder, designed originally
+by genuapp at http://www.genuapp.appspot.com/char/index.html.
 
-sSTR[999]=0;sCON[999]=0;sDEX[999]=0;sQUI[999]=0;sINT[999]=0;sPIE[999]=0;sEMP[999]=0;sCHA[999]=0;
-sSTR[998]=0;sCON[998]=0;sDEX[998]=0;sQUI[998]=0;sINT[998]=0;sPIE[998]=0;sEMP[998]=0;sCHA[998]=0; 
-sSTR[1]=60;sCON[1]=60;sDEX[1]=60;sQUI[1]=60;sINT[1]=60;sPIE[1]=60;sEMP[1]=60;sCHA[1]=60; 
-sSTR[3]=90;sCON[3]=60;sDEX[3]=40;sQUI[3]=40;sINT[3]=60;sPIE[3]=60;sEMP[3]=70;sCHA[3]=60; 
-sSTR[14]=70;sCON[14]=60;sDEX[14]=55;sQUI[14]=45;sINT[14]=70;sPIE[14]=60;sEMP[14]=60;sCHA[14]=60; 
+It has been modified from its original code to suit the purposes of the
+HavenDAoC open source project.
+
+Please note that this is a work-in-progress and available for free distribution
+on an "as-is" basis, without warranties or conditions of any kind, expressed
+or implied.
+************************************************************************* */
+
+/* ************************************************
+ * ANIMIST - HIBERNIA
+ * Class based upon v1.65 patch level.
+************************************************ */
+
+/* ************************************************
+ * Race Selection
+************************************************ */
+
+/* Placeholder Races */
+sSTR[999] = 0;
+sCON[999] = 0;
+sDEX[999] = 0;
+sQUI[999] = 0;
+sINT[999] = 0;
+sPIE[999] = 0;
+sEMP[999] = 0;
+sCHA[999] = 0;
+
+sSTR[998] = 0;
+sCON[998] = 0;
+sDEX[998] = 0;
+sQUI[998] = 0;
+sINT[998] = 0;
+sPIE[998] = 0;
+sEMP[998] = 0;
+sCHA[998] = 0;
+
+// Base stats for the Celt race.
+sSTR[1] = 60;
+sCON[1] = 60;
+sDEX[1] = 60;
+sQUI[1] = 60;
+sINT[1] = 60;
+sPIE[1] = 60;
+sEMP[1] = 60;
+sCHA[1] = 60;
+
+// Base stats for the Firbolg race.
+sSTR[3] = 90;
+sCON[3] = 60;
+sDEX[3] = 40;
+sQUI[3] = 40;
+sINT[3] = 60;
+sPIE[3] = 60;
+sEMP[3] = 70;
+sCHA[3] = 60;
+
+// Base stats for the Sylvan race.
+sSTR[14] = 70;
+sCON[14] = 60;
+sDEX[14] = 55;
+sQUI[14] = 45;
+sINT[14] = 70;
+sPIE[14] = 60;
+sEMP[14] = 60;
+sCHA[14] = 60;
+
+/* ************************************************
+ * Class Skills/Specializations
+************************************************ */
 
 Abilities = new Array(8)		
 Abilities[1]=new abilityPT("Staff",0,83,5,0,0,0)
