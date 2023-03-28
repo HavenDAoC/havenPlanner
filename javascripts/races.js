cedb227e7557ef9ce7473fcb5c43580805263118
deftness <leftygamer02@pm.me>
2023-03-28 13:30:24 -0600

    Added several functions for base stats

---
+++ javascripts/races.js
@@ -0,0 +1,38 @@
+/*! ************************************************************************
+DAOC CHARACTER BUILDER - A static website for planning DAoC character specs
+
+This website is based upon the Uthgard Character Builder, designed originally
+by genuapp at http://www.genuapp.appspot.com/char/index.html.
+
+It has been modified from its original code to suit the purposes of the
+HavenDAoC open source project.
+
+Please note that this is a work-in-progress and available for free distribution
+on an "as-is" basis, without warranties or conditions of any kind, expressed
+or implied.
+************************************************************************* */
+
+/* ************************************************
+ * Functions Containing Racial Base Stats
+************************************************ */
+
+/**
+ * Base stats for the Celt race.
+ */
+function statsCelt() {
+    sSTR[1]=60;sCON[1]=60;sDEX[1]=60;sQUI[1]=60;sINT[1]=60;sPIE[1]=60;sEMP[1]=60;sCHA[1]=60;
+}
+
+/**
+ * Base stats for the Firbolg race.
+ */
+function statsFirbolg() {
+    sSTR[3]=90;sCON[3]=60;sDEX[3]=40;sQUI[3]=40;sINT[3]=60;sPIE[3]=60;sEMP[3]=70;sCHA[3]=60;
+}
+
+/**
+ * Base stats for the Sylvan race.
+ */
+function statsSylvan() {
+    sSTR[14]=70;sCON[14]=60;sDEX[14]=55;sQUI[14]=45;sINT[14]=70;sPIE[14]=60;sEMP[14]=60;sCHA[14]=60;
+}
